a276a0436e4298864bd5b5e0a9446472e8c869b8
Isaia26633 <123019005+Isaia26633@users.noreply.github.com>
2025-02-01 19:31:23 -0500

    Update app.js

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ require('dotenv').config();
 const express = require('express');
 const SpotifyWebApi = require('spotify-web-api-node');
 const app = express();
-const port = 3000;
+const port = process.env.PORT || 3000;
 
 app.set('view engine', 'ejs');
 app.use(express.static('public'));
